a11582e28f545ab129220dccacc7b3e9ca41d621
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-22 19:47:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ class Person {
         this.name = name;
         this.age = age;
     }
-greet() {
-    console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
 }
-}
+
 class Employee extends Person {
     constructor(name, age, jobTitle) {
         super(name, age);
         this.jobTitle = jobTitle;
     }
-jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old and my job title is ${this.jobTitle}.`);
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old and my job title is ${this.jobTitle}.`);
+    }
 }
-}
-let person = new Person("Alice", 25);
-person.greet(); // This should log: "Hello, my name is Alice and I am 25 years old
-let employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet(); // This should log: "Hello, my name is Bob, I am 30 years old and my job title is Manager.
+
 // Do not change the code below this line
 window.Person = Person;
 window.Employee = Employee;
